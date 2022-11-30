589b66fdcf6549bc5f1f5d37a0c8622e93885e4e
licetullmann <licetrosales@gmail.com>
2022-11-30 21:40:55 +0100

    Change test function name

--- Sum.java
+++ Sum.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Sum {
+public class Sum {
+    // public = für alle einsesehbar
+    // static = Methode einer Klasse
+    // int = Datentyp der zurückgegeben wird
+    //sum = Nmae der Methode
+    // (int a, int b) = Parameter der Methode = Daten die wir der Methode geben
+
+    public static int sum(int a, int b){
+        int sumOfAPlusB = a+b;
+        return sumOfAPlusB;
+    }
 }
--- SumTest.java
+++ SumTest.java
@@ -1,4 +1,25 @@
+import org.junit.Test;
+import org.junit.jupiter.api.RepeatedTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SumTest {
-  
+
+    @org.junit.jupiter.api.Test
+// "@Test“ = Annotation = Testmethode, d.h. sie testet eine Methode
+    @Test
+    void sum_should_return_sum_of_a_and_b() {
+
+        // GIVEN = Das was wir am Anfang haben
+        int zahl1 = 5;
+        int zahl2 = 7;
+        int expected = zahl1 + zahl2; // 12
+
+        // WHEN = Das was wir testen wollen, rufen wir hier auf
+        int result = Sum.sum(zahl1, zahl2);
+
+        // THEN = Das Erbegnis das wir erwarten
+        // Assertions  = Sicherstellungen helfen uns Ergebnisse sicherzustellen (probar)
+        assertEquals(expected, result);
+    }
 }
